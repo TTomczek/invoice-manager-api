f1658b960d3703dddc6dd648bcc9f829060d02a9
ttomczek <>
2025-11-22 11:55:45 +0100

    added parameters and xml support

--- generate_win.ps1
+++ generate_win.ps1
@@ -1,13 +1,13 @@
 Write-Output "Generating OpenAPI client on windows"
 ./mvnw.cmd clean install -DskipTests
-Set-Location invoice-manager-api-typescript-angular-client/target/generated-sources/openapi
-Write-Output "Installing angular dependencies"
-npm install
-Write-Output "Building angular api"
-npm run build
-Set-Location dist
-Write-Output "Packing angular api"
-npm pack
-Set-Location ../../../../..
-Write-Output "Install angular api with: npm i '..\invoice-manager-api\invoice-manager-api-typescript-angular-client\target\generated-sources\openapi\dist\invoice-manager-api-typescript-angular-client-1.0.0-SNAPSHOT.tgz'"
+# Set-Location invoice-manager-api-typescript-angular-client/target/generated-sources/openapi
+# Write-Output "Installing angular dependencies"
+# npm install
+# Write-Output "Building angular api"
+# npm run build
+# Set-Location dist
+# Write-Output "Packing angular api"
+# npm pack
+# Set-Location ../../../../..
+# Write-Output "Install angular api with: npm i '..\invoice-manager-api\invoice-manager-api-typescript-angular-client\target\generated-sources\openapi\dist\invoice-manager-api-typescript-angular-client-1.0.0-SNAPSHOT.tgz'"
 Write-Output "Done"
